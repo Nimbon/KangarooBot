6f1614711e74e3198c6e789d9f88865b4182959f
Nimbon <33168771+Nimbon@users.noreply.github.com>
2018-10-03 12:19:30 +1000

    Delete circle.sh

--- circle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo apt-get remove mysql* -y
-sudo apt-get autoremove -y
-sudo apt-get update -y
-sudo apt-get dist-upgrade -y
-sudo apt-get install lxde-core tightvncserver screen unifont firefox aria2 make nano -y
