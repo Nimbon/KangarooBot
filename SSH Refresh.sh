58ccad9a11befd9f702f4cacaba58ce8579c7ee4
Nimbon <33168771+Nimbon@users.noreply.github.com>
2019-10-04 14:45:02 +1000

    Update stretch to buster
    Change code for new debian.

--- SSH Refresh.sh
+++ SSH Refresh.sh
@@ -3,22 +3,22 @@
 rm -rf /etc/ssh/ssh_host_*
 ssh-keygen -A
 service ssh restart
-#Add User kangaroo
-adduser kangaroo --gecos "" --disabled-password
-echo "kangaroo:placeholderpass" | chpasswd
-adduser kangaroo sudo
+#Add User Nimbon
+adduser Nimbon --gecos "" --disabled-password
+echo "Nimbon:placeholderpwd" | chpasswd
+adduser Nimbon sudo
 passwd -l root
 passwd -d root
 #Remove apt-listchanges
 apt-get remove apt-listchanges -y
 #Fix Mirrors
 cat > /etc/apt/sources.list << SOURCES
-deb http://http.debian.net/debian stretch main contrib non-free
-deb-src http://http.debian.net/debian stretch main contrib non-free
-deb http://security.debian.org/ stretch/updates main contrib non-free
-deb-src http://security.debian.org/ stretch/updates main contrib non-free
-deb http://http.debian.net/debian stretch-updates main contrib non-free
-deb-src http://http.debian.net/debian stretch-updates main contrib non-free
+deb http://http.debian.net/debian buster main contrib non-free
+deb-src http://http.debian.net/debian buster main contrib non-free
+deb http://security.debian.org/ buster/updates main contrib non-free
+deb-src http://security.debian.org/ buster/updates main contrib non-free
+deb http://http.debian.net/debian buster-updates main contrib non-free
+deb-src http://http.debian.net/debian buster-updates main contrib non-free
 SOURCES
 #Update System First
 apt-get update && apt-get dist-upgrade -y
